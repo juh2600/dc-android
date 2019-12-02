99edad08d99ec045aabde42ba681322735842067
Joseph Reed <josephreed2600@gmail.com>
2019-12-01 22:17:30 -0700

    revamp some arithmetic for rounding stuff

--- app/src/main/java/com/example/dc/Calculator.java
+++ app/src/main/java/com/example/dc/Calculator.java
@@ -1,45 +1,77 @@
 package com.example.dc;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Stack;
 
 public class Calculator {
 
-    public void performOperation(Stack<Character> stack) {
-        switch(stack.pop()) {
-            case '+':
-                //TODO: Add
-                break;
-            case '-':
-                //TODO: Subtract
-                break;
-            case '*':
-                //TODO: Multiply
-                break;
-            case '/':
-                //TODO: Divide
-                break;
-        }
+    private MathContext context;
+
+    public Calculator() {
+        context = new MathContext(0, RoundingMode.DOWN);
     }
 
-    public BigDecimal add(BigDecimal a, BigDecimal b) {
+    public void setPrecision(int k) {
+        context = new MathContext(k, RoundingMode.DOWN);
+    }
+
+    public int getPrecision() {
+        return context.getPrecision();
+    }
+
+    public BigDecimal add(BigDecimal b, BigDecimal a) {
         return a.add(b);
     }
 
-    public BigDecimal subtract(BigDecimal a, BigDecimal b) {
+    /**
+     * Subtracts one number from another.
+     * @param b subtrahend
+     * @param a minuend
+     * @return a - b
+     */
+    public BigDecimal subtract(BigDecimal b, BigDecimal a) {
         return a.subtract(b);
     }
 
-    public BigDecimal multiply(BigDecimal a, BigDecimal b) {
-        return a.multiply(b);
+    /**
+     * Multiply two numbers, keeping the scale of the number whose scale is larger.
+     */
+    public BigDecimal multiply(BigDecimal b, BigDecimal a) {
+        int precisionA = a.precision() - a.scale();
+        int precisionB = b.precision() - b.scale();
+        int precision = Math.max(precisionA, precisionB);
+        int scaleA = a.scale();
+        int scaleB = b.scale();
+        int scale = Math.max(scaleA, scaleB);
+        return a.multiply(b, new MathContext(scale + precision, RoundingMode.DOWN));
     }
 
-    public BigDecimal divide(BigDecimal a, BigDecimal b) {
-        return a.divide(b);
+    /**
+     * Divide one number by another, using the current precision value as the scale of the quotient.
+     * @param b Divisor
+     * @param a Dividend
+     * @return a / b
+     */
+    public BigDecimal divide(BigDecimal b, BigDecimal a) {
+            return a.divide(b, context).setScale(context.getPrecision(), context.getRoundingMode());
     }
 
-    public BigDecimal mod(BigDecimal a, BigDecimal b) {
+    /**
+     * According to the dc manual, the modulus is equivalent to the remainder
+     * of the division that the regular division operator would perform.
+     * If we are working in a precision greater than zero, this may produce
+     * initially unexpected results. For example, 1%2 gives 1 when precision
+     * is zero, but for any greater precision, 1/2 yields 0.5, so 1%2 gives 0.
+     * @param b Modulus
+     * @param a Dividend
+     * @return a % b
+     */
+    public BigDecimal mod(BigDecimal b, BigDecimal a) {
+        return a.subtract(a.divide(b, context).multiply(b));
         //I just woke up but i think this works??
+        /*
         BigDecimal remainder = a.subtract(b);
         while(remainder.compareTo(BigDecimal.ZERO) > 0) {
             if(remainder.subtract(b).compareTo(BigDecimal.ZERO) < 0) {
@@ -49,5 +81,6 @@ public class Calculator {
             }
         }
         return remainder;
+         */
     }
 }
